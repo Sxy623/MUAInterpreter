8a79d2a3c4305be0fc0c8c896d052f61a9720011
Sxy623 <191436989@qq.com>
2019-11-17 21:33:39 +0800

    Add list and type checking

--- Main.java
+++ Main.java
@@ -18,33 +18,64 @@ public class Main {
 
 	public static double eps = 1e-5;
 	public static Scanner scan = new Scanner(System.in);
-	// 变量空间
+	
+	// 全局变量空间
 	public static Map<String, Variable> map = new HashMap<String, Variable>();
 
-	// 判断是否为Word字面量
+	// 判断是否为 Word 字面量
 	public static boolean isWordLiteral(String str) {
 		return str.charAt(0) == '\"';
 	}
-	
-	// 从Word字面量中得到Word
+	// 从 Word 字面量中得到 Word 变量的 content
 	public static String getWord(String str) {
 		return str.substring(1);
 	}
 	
-	// 判断是否为Number字面量
-	public static boolean isNumLiteral(String str) {
+	// 判断是否为 Number 字面量
+	public static boolean isNumberLiteral(String str) {
 		return str.charAt(0) == '-' || (str.charAt(0) >= '0' && str.charAt(0) <= '9');
 	}
-	
-	// 从Number字面量中得到Number
-	public static String getNum(String str) {
+	// 从 Number 字面量中得到 Number 变量的 content
+	public static String getNumber(String str) {
 		return Double.valueOf(str).toString();
 	}
 	
+	// 判断是否为 Boolean 字面量
+	public static boolean isBooleanLiteral(String str) {
+		return str.equals("true") || str.equals("false");
+	}
+	// 从 Boolean 字面量中得到 Boolean 变量的 content
+	public static String getBoolean(String str) {
+		return str;
+	}
+	
+	// 判断是否为 List 字面量
+	public static boolean isListLiteral(String str) {
+		return str.charAt(0) == '[';
+	}
+	// 读到 List 字面量结束，返回 List 变量的 content
+	public static String getList(String str) {
+		if (str.contains("]")) {
+			int index = str.lastIndexOf("]");
+			return str.substring(1, index);
+		}
+		
+		String tempStr = str.substring(1);
+		String p = scan.next();
+		while(!p.contains("]")) {
+			tempStr += " " + p;
+			p = scan.next();
+		}
+		int index = p.lastIndexOf("]");
+		tempStr += " " + p.substring(0, index);
+		return tempStr;
+	}
+	
+	// 判断是否以冒号开头
 	public static boolean hasColon(String str) {
 		return str.charAt(0) == ':';
 	}
-	
+	// 对冒号进行递归解析，取出对应变量
 	public static Variable parseColon(String str) {
 		String id = str.substring(1);
 		if (hasColon(id)) {
@@ -125,101 +156,176 @@ public class Main {
 		return new Variable(result.toString(), Type.BOOLEAN);
 	}
 	
-	public static Variable nextParameter() {
+	// 判断类型
+	// 返回值: Boolean类型的Variable
+	public static Variable isNumber(Variable a) {
+		Boolean result = (a.type == Type.NUMBER);
+		return new Variable(result.toString(), Type.BOOLEAN);
+	}
+	public static Variable isWord(Variable a) {
+		Boolean result = (a.type == Type.WORD);
+		return new Variable(result.toString(), Type.BOOLEAN);
+	}
+	public static Variable isList(Variable a) {
+		Boolean result = (a.type == Type.LIST);
+		return new Variable(result.toString(), Type.BOOLEAN);
+	}
+	public static Variable isBool(Variable a) {
+		Boolean result = (a.type == Type.BOOLEAN);
+		return new Variable(result.toString(), Type.BOOLEAN);
+	}
+	public static Variable isEmpty(Variable a) {
+		Boolean result = a.content.isEmpty();
+		return new Variable(result.toString(), Type.BOOLEAN);
+	}
+	
+	public static Variable nextParameter(Scanner scan) {
 		String p = scan.next();
+		// 解析 Word 字面量
 		if (isWordLiteral(p)) {
 			return new Variable(getWord(p), Type.WORD);
 		}
-		else if (isNumLiteral(p)) {
-			return new Variable(getNum(p), Type.NUMBER);
+		// 解析 Number 字面量
+		else if (isNumberLiteral(p)) {
+			return new Variable(getNumber(p), Type.NUMBER);
 		}
+		// 解析 Boolean 字面量
+		else if (isBooleanLiteral(p)) {
+			return new Variable(getBoolean(p), Type.BOOLEAN);
+		}
+		// 解析 List 字面量
+		else if (isListLiteral(p)) {
+			return new Variable(getList(p), Type.LIST);
+		}
+		// 解析冒号
 		else if (hasColon(p)) {
 			return parseColon(p);
 		}
 		
 		Variable p1, p2;
+		
 		switch(p) {
 		case "thing":
-			p1 = nextParameter();
+			p1 = nextParameter(scan);
 			Variable variable = map.get(p1.content);
 			return variable;
 		case "read":
 			p = scan.next();
 			return new Variable(p, Type.WORD);
 		case "isname":
-			p1 = nextParameter();
+			p1 = nextParameter(scan);
 			if (map.get(p1.content) == null) {
 				return new Variable("false", Type.BOOLEAN);
 			}
 			else {
 				return new Variable("true", Type.BOOLEAN);
 			}
+		// 算术运算
 		case "add":
-			p1 = nextParameter();
-			p2 = nextParameter();
+			p1 = nextParameter(scan);
+			p2 = nextParameter(scan);
 			return addOp(p1, p2);
 		case "sub":
-			p1 = nextParameter();
-			p2 = nextParameter();
+			p1 = nextParameter(scan);
+			p2 = nextParameter(scan);
 			return subOp(p1, p2);
 		case "mul":
-			p1 = nextParameter();
-			p2 = nextParameter();
+			p1 = nextParameter(scan);
+			p2 = nextParameter(scan);
 			return mulOp(p1, p2);
 		case "div":
-			p1 = nextParameter();
-			p2 = nextParameter();
+			p1 = nextParameter(scan);
+			p2 = nextParameter(scan);
 			return divOp(p1, p2);
+		// 比较运算
 		case "eq":
-			p1 = nextParameter();
-			p2 = nextParameter();
+			p1 = nextParameter(scan);
+			p2 = nextParameter(scan);
 			return eqOp(p1, p2);
 		case "gt":
-			p1 = nextParameter();
-			p2 = nextParameter();
+			p1 = nextParameter(scan);
+			p2 = nextParameter(scan);
 			return gtOp(p1, p2);
 		case "lt":
-			p1 = nextParameter();
-			p2 = nextParameter();
+			p1 = nextParameter(scan);
+			p2 = nextParameter(scan);
 			return ltOp(p1, p2);
+		// 逻辑运算
 		case "and":
-			p1 = nextParameter();
-			p2 = nextParameter();
+			p1 = nextParameter(scan);
+			p2 = nextParameter(scan);
 			return andOp(p1, p2);
 		case "or":
-			p1 = nextParameter();
-			p2 = nextParameter();
+			p1 = nextParameter(scan);
+			p2 = nextParameter(scan);
 			return orOp(p1, p2);
 		case "not":
-			p1 = nextParameter();
+			p1 = nextParameter(scan);
 			return notOp(p1);
 		case "readlist":
+			p = scan.nextLine();  // 行末回车
 			p = scan.nextLine();
 			return new Variable(p, Type.LIST);
+		// 判断类型
+		case "isnumber":
+			p1 = nextParameter(scan);
+			return isNumber(p1);
+		case "isword":
+			p1 = nextParameter(scan);
+			return isWord(p1);
+		case "islist":
+			p1 = nextParameter(scan);
+			return isList(p1);
+		case "isbool":
+			p1 = nextParameter(scan);
+			return isBool(p1);
+		case "isempty":
+			p1 = nextParameter(scan);
+			return isEmpty(p1);
 		}
 		return null;
 	}
 	
-	public static void main(String[] args) {
+	public static void interpret(Scanner scan) {
+		Scanner tempScanner;
 		while (scan.hasNext()) {
 			String instruction = scan.next();
 			Variable p1, p2;
 			switch(instruction) {
 			case "make":
-				p1 = nextParameter();
-				p2 = nextParameter();
+				p1 = nextParameter(scan);
+				p2 = nextParameter(scan);
 				map.put(p1.content, p2);
 				break;
 			case "print":
-				p1 = nextParameter();
+				p1 = nextParameter(scan);
 				System.out.println(p1.content);
 				break;
 			case "erase":
-				p1 = nextParameter();
+				p1 = nextParameter(scan);
 				map.remove(p1.content);
+				break;
+			case "run":
+				p1 = nextParameter(scan);
+				tempScanner = new Scanner(p1.content);
+				interpret(tempScanner);
+				break;
+			case "repeat":
+				p1 = nextParameter(scan);
+				p2 = nextParameter(scan);
+				double num = Double.valueOf(p2.content);
+				for (int i = 0; i < num - eps; i++) {
+					tempScanner = new Scanner(p1.content);
+					interpret(tempScanner);
+				}
 				break;
 			}
 		}
 	}
 	
+	public static void main(String[] args) {
+		while (scan.hasNext()) {
+			interpret(scan);
+		}
+	}
 }
